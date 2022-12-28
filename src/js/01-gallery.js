334854ae897592d15c343c7dc5129e3a9daac606
Andrey <49126678+Minaht@users.noreply.github.com>
2022-12-28 10:25:04 +0200

    gallery start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleries = document.querySelector(".gallery")
+    const galleriItem = createGalleryItemsMarkup(galleryItems); 
+    function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a href="" class="gallery__link">
+                <img
+                    class="gallery__image" 
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join("");
+ 
+    }
+// console.log(createGalleryItemsMarkup(galleryItems))
+
+galleries.insertAdjacentHTML("afterbegin",galleriItem);
